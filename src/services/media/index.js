ab02c98fb3700bc56bc6dce29f0ff8d6cba5375c
Code4Wyatt <44061707+Code4Wyatt@users.noreply.github.com>
2021-10-29 12:33:08 +0100

    Added error handlers

--- src/services/media/index.js
+++ src/services/media/index.js
@@ -25,6 +25,8 @@ const __dirname = dirname(__filename);
 
 const mediaRouter = express.Router();
 
+
+
 // get all media
 mediaRouter.get("/media", async (req, res, next) => {
   try {
@@ -36,6 +38,7 @@ mediaRouter.get("/media", async (req, res, next) => {
     res.send(500).send({ message: error.message });
   }
 });
+
 
 
 // post media
@@ -66,10 +69,12 @@ mediaRouter.post(
   }
 );
 
+
+
 // get single media
 mediaRouter.get("/media/:id", async (req, res, next) => {
   try {
-    const fileAsBuffer = fs.readFileSync(blogsFilePath);
+    const fileAsBuffer = fs.readFileSync(mediaFilePath);
 
     const fileAsString = fileAsBuffer.toString();
 
@@ -81,58 +86,13 @@ mediaRouter.get("/media/:id", async (req, res, next) => {
         .status(404)
         .send({ message: `media with ${req.params.id} is not found!` });
     }
-    res.send(blog);
+    res.send(media);
   } catch (error) {
     res.send(500).send({ message: error.message });
   }
 });
 
-mediaRouter.get("/:id/comments", async (req, res, next) => {
-  try {
-    const fileAsBuffer = fs.readFileSync(blogsFilePath);
 
-    const fileAsString = fileAsBuffer.toString();
-
-    const fileAsJSONArray = JSON.parse(fileAsString);
-
-    const blog = fileAsJSONArray.find((blog) => blog.id === req.params.id);
-    if (!blog) {
-      res
-        .status(404)
-        .send({ message: `blog with ${req.params.id} is not found!` });
-    }
-
-    blog.comments = blog.comments || [];
-    res.send(blog.comments);
-  } catch (error) {
-    res.send(500).send({ message: error.message });
-  }
-});
-
-// delete  blog
-mediaRouter.delete("/:id", async (req, res, next) => {
-  try {
-    const fileAsBuffer = fs.readFileSync(blogsFilePath);
-
-    const fileAsString = fileAsBuffer.toString();
-
-    let fileAsJSONArray = JSON.parse(fileAsString);
-
-    const blog = fileAsJSONArray.find((blog) => blog.id === req.params.id);
-    if (!blog) {
-      res
-        .status(404)
-        .send({ message: `blog with ${req.params.id} is not found!` });
-    }
-    fileAsJSONArray = fileAsJSONArray.filter(
-      (blog) => blog.id !== req.params.id
-    );
-    fs.writeFileSync(blogsFilePath, JSON.stringify(fileAsJSONArray));
-    res.status(204).send();
-  } catch (error) {
-    res.send(500).send({ message: error.message });
-  }
-});
 
 //  update media
 mediaRouter.put("/media/:id", async (req, res, next) => {
@@ -167,93 +127,7 @@ mediaRouter.put("/media/:id", async (req, res, next) => {
   }
 });
 
-// REVIEW post
 
-mediaRouter.post(
-  "/media/:id/review",
-//   checkReviewSchema,
-//   checkValidationResult,
-  async (req, res, next) => {
-    try {
-      const { text, userName } = req.body;
-      const review = { id: uniqid(), comment, rate, elementId, createdAt: new Date() };
-      const fileAsBuffer = fs.readFileSync(mediaFilePath);
-
-      const fileAsString = fileAsBuffer.toString();
-
-      let fileAsJSONArray = JSON.parse(fileAsString);
-
-      const mediaIndex = fileAsJSONArray.findIndex(
-        (media) => media.id === req.params.id
-      );
-      if (!mediaIndex == -1) {
-        res
-          .status(404)
-          .send({ message: `media with ${req.params.id} is not found!` });
-      }
-      const previousmediaData = fileAsJSONArray[mediaIndex];
-      previousmediaData.comments = previousmediaData.comments || [];
-      const changedmedia = {
-        ...previousmediaData,
-        ...req.body,
-        comments: [...previousmediaData.comments, comment],
-        updatedAt: new Date(),
-        id: req.params.id,
-      };
-      fileAsJSONArray[mediaIndex] = changedmedia;
-
-      fs.writeFileSync(mediaFilePath, JSON.stringify(fileAsJSONArray));
-      res.send(changedmedia);
-    } catch (error) {
-      console.log(error);
-      res.send(500).send({ message: error.message });
-    }
-  }
-);
-
-// REVIEW edit
-
-mediaRouter.put(
-  "/media/:id/review",
-//   checkReviewSchema,
-//   checkValidationResult,
-  async (req, res, next) => {
-    try {
-      const { text, userName } = req.body;
-      const review = { id: uniqid(), comment, rate, elementId, createdAt: new Date() };
-      const fileAsBuffer = fs.readFileSync(mediaFilePath);
-
-      const fileAsString = fileAsBuffer.toString();
-
-      let fileAsJSONArray = JSON.parse(fileAsString);
-
-      const mediaIndex = fileAsJSONArray.findIndex(
-        (media) => media.id === req.params.id
-      );
-      if (!mediaIndex == -1) {
-        res
-          .status(404)
-          .send({ message: `media with ${req.params.id} is not found!` });
-      }
-      const previousmediaData = fileAsJSONArray[mediaIndex];
-      previousmediaData.comments = previousmediaData.comments || [];
-      const changedmedia = {
-        ...previousmediaData,
-        ...req.body,
-        comments: [...previousmediaData.comments, comment],
-        updatedAt: new Date(),
-        id: req.params.id,
-      };
-      fileAsJSONArray[mediaIndex] = changedmedia;
-
-      fs.writeFileSync(mediaFilePath, JSON.stringify(fileAsJSONArray));
-      res.send(changedmedia);
-    } catch (error) {
-      console.log(error);
-      res.send(500).send({ message: error.message });
-    }
-  }
-);
 
 // POSTER postage
 
@@ -292,5 +166,7 @@ mediaRouter.post(
     }
   }
 );
+
+
 
 export default mediaRouter;
---
+++ src/services/reviews/index.js
@@ -0,0 +1,142 @@
+import express from "express";
+
+import fs from "fs";
+
+import uniqid from "uniqid";
+
+import path, { dirname } from "path";
+
+import { fileURLToPath } from "url";
+
+import { parseFile, uploadFile } from "../../utils/upload/index.js";
+
+// import {
+//   checkBlogPostSchema,
+//   checkCommentSchema,
+//   checkSearchSchema,
+//   checkValidationResult,
+// } from "./validation.js";
+
+import { reviewsFilePath } from "../../utils/upload/index.js";
+
+const __filename = fileURLToPath(import.meta.url);
+
+const __dirname = dirname(__filename);
+
+const reviewRouter = express.Router();
+
+// REVIEW post
+
+reviewRouter.post(
+  "/media/:id/review",
+//   checkReviewSchema,
+//   checkValidationResult,
+  async (req, res, next) => {
+    try {
+      const { text, userName } = req.body;
+      const review = { id: uniqid(), comment, rate, elementId, createdAt: new Date() };
+      const fileAsBuffer = fs.readFileSync(mediaFilePath);
+
+      const fileAsString = fileAsBuffer.toString();
+
+      let fileAsJSONArray = JSON.parse(fileAsString);
+
+      const mediaIndex = fileAsJSONArray.findIndex(
+        (media) => media.id === req.params.id
+      );
+      if (!mediaIndex == -1) {
+        res
+          .status(404)
+          .send({ message: `media with ${req.params.id} is not found!` });
+      }
+      const previousmediaData = fileAsJSONArray[mediaIndex];
+      previousmediaData.comments = previousmediaData.comments || [];
+      const changedmedia = {
+        ...previousmediaData,
+        ...req.body,
+        comments: [...previousmediaData.comments, comment],
+        updatedAt: new Date(),
+        id: req.params.id,
+      };
+      fileAsJSONArray[mediaIndex] = changedmedia;
+
+      fs.writeFileSync(reviewsFilePath, JSON.stringify(fileAsJSONArray));
+      res.send(changedmedia);
+    } catch (error) {
+      console.log(error);
+      res.send(500).send({ message: error.message });
+    }
+  }
+);
+
+// REVIEW edit
+
+reviewRouter.put(
+  "/media/:id/review",
+//   checkReviewSchema,
+//   checkValidationResult,
+  async (req, res, next) => {
+    try {
+      const { text, userName } = req.body;
+      const review = { id: uniqid(), comment, rate, elementId, createdAt: new Date() };
+      const fileAsBuffer = fs.readFileSync(mediaFilePath);
+
+      const fileAsString = fileAsBuffer.toString();
+
+      let fileAsJSONArray = JSON.parse(fileAsString);
+
+      const mediaIndex = fileAsJSONArray.findIndex(
+        (media) => media.id === req.params.id
+      );
+      if (!mediaIndex == -1) {
+        res
+          .status(404)
+          .send({ message: `media with ${req.params.id} is not found!` });
+      }
+      const previousmediaData = fileAsJSONArray[mediaIndex];
+      previousmediaData.comments = previousmediaData.comments || [];
+      const changedmedia = {
+        ...previousmediaData,
+        ...req.body,
+        comments: [...previousmediaData.comments, comment],
+        updatedAt: new Date(),
+        id: req.params.id,
+      };
+      fileAsJSONArray[mediaIndex] = changedmedia;
+
+      fs.writeFileSync(mediaFilePath, JSON.stringify(fileAsJSONArray));
+      res.send(changedmedia);
+    } catch (error) {
+      console.log(error);
+      res.send(500).send({ message: error.message });
+    }
+  }
+);
+
+// REVIEW delete
+
+reviewRouter.delete("/media/:id/review", async (req, res, next) => {
+  try {
+    const fileAsBuffer = fs.readFileSync(mediaFilePath);
+
+    const fileAsString = fileAsBuffer.toString();
+
+    let fileAsJSONArray = JSON.parse(fileAsString);
+
+    const media = fileAsJSONArray.find((media) => media.id === req.params.id);
+    if (!media) {
+      res
+        .status(404)
+        .send({ message: `media with ${req.params.id} is not found!` });
+    }
+    fileAsJSONArray = fileAsJSONArray.filter(
+      (media) => media.id !== req.params.id
+    );
+    fs.writeFileSync(mediaFilePath, JSON.stringify(fileAsJSONArray));
+    res.status(204).send();
+  } catch (error) {
+    res.send(500).send({ message: error.message });
+  }
+});
+
+export default reviewRouter;
